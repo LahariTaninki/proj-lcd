b1ec5e919479d78d0bc48b1b6d15308044c14949
LahariTaninki <98213477+LahariTaninki@users.noreply.github.com>
2024-02-26 21:30:09 +0530

    Update Lungclassify.py

--- pages/Lungclassify.py
+++ pages/Lungclassify.py
@@ -4,50 +4,46 @@ from PIL import Image, ImageOps
 from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.mobilenet import preprocess_input
 
-# Define the Streamlit page configuration
+# Set page configuration
 st.set_page_config(
-    page_title="VLP",
+    page_title="Vitality Lung Predictor",
     initial_sidebar_state="expanded",
 )
 
-# Hide the Streamlit menu and footer
-st.markdown(""" <style>
+# Hide Streamlit footer and menu
+st.markdown("""
+<style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
-</style> """, unsafe_allow_html=True)
+</style>
+""", unsafe_allow_html=True)
 
-# Load the lung cancer prediction model
-model = load_model('models/LCC.h5', compile=False)
-
-# Define custom class labels for prediction
-custom_class_labels = ["Adenocarcinoma", "Large cell Carcinoma", "Normal", "Squamous"]
-
-# Title of the application
-st.title("Vitality Lung Predictor")
-
-# File uploader for user to upload an image
+# File uploader
 uploaded_file = st.file_uploader("Choose a File", type=['jpg', 'png', 'jpeg'])
 
-# Button to trigger the prediction process
-if st.button("Predict"):
-    # Check if an image is uploaded
-    if uploaded_file is not None:
-        # Open and preprocess the uploaded image
-        image = Image.open(uploaded_file)
-        size = (224, 224)
-        image = ImageOps.fit(image, size)
-        image_array = np.asarray(image)
-        image_array = np.expand_dims(image_array, axis=0)
-        image_array = preprocess_input(image_array)
-        
-        # Make predictions using the loaded model
-        predictions = model.predict(image_array)
-        predicted_class_index = np.argmax(predictions)
-        predicted_class_label = custom_class_labels[predicted_class_index]
+# Load the model
+model = load_model('models/LCC.h5', compile=False)
 
-        # Display the predicted class index and label
-        st.write("Predicted Class Index:", predicted_class_index)
-        st.write("Predicted Class Label:", predicted_class_label)
-    else:
-        # Display a message if no image is uploaded
-        st.error("Please upload an image first.")
+# Function to preprocess the image
+def preprocess_image(image):
+    size = (224, 224)
+    image = image.convert("RGB")  # Convert RGBA image to RGB
+    image = ImageOps.fit(image, size)
+    image_array = np.asarray(image)
+    image_array = np.expand_dims(image_array, axis=0)
+    return preprocess_input(image_array)
+
+# Predict button
+if uploaded_file is not None:
+    st.image(uploaded_file)
+    predict_button = st.button("Predict")
+    if predict_button:
+        with st.spinner("Diagnosing..."):
+            image = Image.open(uploaded_file)
+            processed_image = preprocess_image(image)
+            predictions = model.predict(processed_image)
+            predicted_class_index = np.argmax(predictions)
+            custom_class_labels = ["Adenocarcinoma", "Large cell Carcinoma", "Normal", "Squamous"]
+            predicted_class_label = custom_class_labels[predicted_class_index]
+            st.write("Predicted Class Index:", predicted_class_index)
+            st.write("Predicted Class Label:", predicted_class_label)
